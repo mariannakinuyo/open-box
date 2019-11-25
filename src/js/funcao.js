55a58a26fcbfcedcdfb21244598dc70763b526eb
Henrique Rckt <henrique@rckt.com.br>
2019-11-24 22:37:46 -0300

    add vue / andar os circulos no final

---
+++ src/js/funcao.js
@@ -0,0 +1,102 @@
+// math trick 2*pi*57 = 358, must be less than 360 degree
+var pointer = document.getElementById('pointer');
+var myTimer = document.getElementById('myTimer');
+
+// movimento
+var interval = 30;
+var angle = 0;
+var angle_increment = 6;
+
+var arrayFederal = pessoas.nodes.filter(e => {
+    return e.type == 'federal';
+});
+var arrayEstadual = pessoas.nodes.filter(e => {
+    return e.type == 'estadual';
+});
+var arrayPrivada = pessoas.nodes.filter(e => {
+    return e.type == 'privada';
+});
+var arrayExterior = pessoas.nodes.filter(e => {
+    return e.type == 'exterior';
+});
+
+arrayExterior.forEach((el, index) => {
+    var um = document.getElementById('exterior-'+el.id);
+    var degrees = el.degrees; // puxar da tabela
+    var num = degrees*2.15; // degrees * 2.15 - trocar o 100 por degrees
+    var max_angle = Math.round(num); // posição final da bolinha
+
+    window.timer = window.setInterval(function () {
+        var cos = Math.cos(num/180*Math.PI);
+        var sen = Math.sin(num/180*Math.PI);
+        myTimer.innerHTML = parseInt(angle/360*100) + '%';
+        myTimer.setAttribute("fill", "hsl(" + parseInt(angle/360*100) + ", 90%, 45%)");
+        um.setAttribute("cx", 450.504 + (sen * 123.842));
+        um.setAttribute("cy", 369.555 - (cos * 123.842));
+        if (angle >= max_angle) {
+            window.clearInterval(window.timer);
+        };
+        // angle += angle_increment;
+    }.bind(this), interval);
+
+});
+
+arrayPrivada.forEach((el, index) => {
+    var um = document.getElementById('privada-'+el.id);
+    var degrees = el.degrees; // puxar da tabela
+    var num = degrees*1.75; // degrees * 2.15 - trocar o 100 por degrees
+    var max_angle = Math.round(num); // posição final da bolinha
+    var cos = Math.cos(num/180*Math.PI);
+    var sen = Math.sin(num/180*Math.PI);
+
+    window.timer = window.setInterval(function () {
+        myTimer.innerHTML = parseInt(angle/360*100) + '%';
+        myTimer.setAttribute("fill", "hsl(" + parseInt(angle/360*100) + ", 90%, 45%)");
+        um.setAttribute("cx", 449.447 + (sen * 185.611));
+        um.setAttribute("cy", 369.552 - (cos * 185.611));
+        if (angle >= max_angle) {
+            window.clearInterval(window.timer);
+        };
+        // angle += angle_increment;
+    }.bind(this), interval);
+});
+
+arrayEstadual.forEach((el, index) => {
+    var um = document.getElementById('estadual-'+el.id);
+    var degrees = el.degrees; // puxar da tabela
+    var num = degrees*1.75; // degrees * 2.15 - trocar o 100 por degrees
+    var max_angle = Math.round(num); // posição final da bolinha
+    var cos = Math.cos(num/180*Math.PI);
+    var sen = Math.sin(num/180*Math.PI);
+
+    window.timer = window.setInterval(function () {
+        myTimer.innerHTML = parseInt(angle/360*100) + '%';
+        myTimer.setAttribute("fill", "hsl(" + parseInt(angle/360*100) + ", 90%, 45%)");
+        um.setAttribute("cx", 450.5 + (sen * 308.611));
+        um.setAttribute("cy", 368.5 - (cos * 308.611));
+        if (angle >= max_angle) {
+            window.clearInterval(window.timer);
+        };
+        // angle += angle_increment;
+    }.bind(this), interval);
+});
+
+arrayFederal.forEach((el, index) => {
+    var um = document.getElementById('federal-'+el.id);
+    var degrees = el.degrees; // puxar da tabela
+    var num = degrees*1.75; // degrees * 2.15 - trocar o 100 por degrees
+    var max_angle = Math.round(num); // posição final da bolinha
+    var cos = Math.cos(num/180*Math.PI);
+    var sen = Math.sin(num/180*Math.PI);
+
+    window.timer = window.setInterval(function () {
+        myTimer.innerHTML = parseInt(angle/360*100) + '%';
+        myTimer.setAttribute("fill", "hsl(" + parseInt(angle/360*100) + ", 90%, 45%)");
+        um.setAttribute("cx", 450.219 + (sen * 247.746));
+        um.setAttribute("cy", 368.363 - (cos * 247.746));
+        if (angle >= max_angle) {
+            window.clearInterval(window.timer);
+        };
+        // angle += angle_increment;
+    }.bind(this), interval);
+});
--- src/js/script-top.js
+++ src/js/script-top.js
@@ -1,96 +1,104 @@
-// js
-  console.log('geral');
-  // import pessoas from './complete.js';
-  // console.log(pessoas.g);
-  console.log("olar");
+var app = new Vue({
+    el: '#open-box',
+    data: {
+        arrayFederal: [],
+        arrayEstadual: [],
+        arrayPrivada: [],
+        arrayExterior: [],
+    },
 
-  // var novoArray = [];
-  // pessoas.nodes.forEach(function(item, index) {
-  //   let aux = {};
-  //   aux.id = index;
-  //   aux.cx = "415";
-  //   aux.cy = "120";
-  //   aux.r = item.size;
-  //   novoArray.push(aux);
-  // });
+    mounted() {
+        var _this = this;
+
+        this.arrayFederal = pessoas.nodes.filter(e => {
+            return e.type == 'federal';
+        });
+        this.arrayEstadual = pessoas.nodes.filter(e => {
+            return e.type == 'estadual';
+        });
+        this.arrayPrivada = pessoas.nodes.filter(e => {
+            return e.type == 'privada';
+        });
+        this.arrayExterior = pessoas.nodes.filter(e => {
+            return e.type == 'exterior';
+        });
+
+        // declaracao de algumas variaveis necessarias
+        var blue = "rgba(0, 0, 255, 0.5)",
+            red = "rgba(255, 0, 0, 0.5)",
+            yellow = "rgba(255, 255, 0, 0.5)",
+            purple = "rgba(194, 74, 255, 0.5)",
+            green = "rgba(0, 255, 0, 0.5)",
+            g;
+
+    },
+
+    methods: {
+        bolinhasNoMapa: function () {
+
+            
+
+        // // id de cada circle - puxar da tabela
+        //     var two = document.getElementById('federal-');
+        //     var tres = document.getElementById('tres');
+        //     var four = document.getElementById('four');
+        //     var um = document.getElementById('um');
+
+        // // posição final das bolinhas - premios
+            // var degrees = degrees; // puxar da tabela
+            // var num = 100*2.15; // degrees * 2.15 - trocar o 100 por degrees
+            // var max_angle = Math.round(num); // posição final da bolinha
+
+        // // tamanho da bolinha - artigos
+        //     var size = size; // puxar da tabela
+        //     //  var numsize = 50*0.14;  // size * 0.14 - trocar o 50 por size
+        //     //  var finalsize = Math.round(numsize); // tamanho da bolinha
+
+        // // cor da bolinha
+        //     var color = color; // puxar da tabela
+
+        //     window.timer = window.setInterval(function () {
+        //         var cos = Math.cos(angle/180*Math.PI);
+        //         var sen = Math.sin(angle/180*Math.PI);
+        //         //  console.log(angle)
+        //         //  console.log(cos)
+
+        // // (federais) primeiro circulo de fora - cx e cy posiçao inicial da bolinha no circulo de fora, calcula com o raio do circulo de fora
+        //         um.setAttribute("cx", 450.504 + (sen * 123.842));
+        //         um.setAttribute("cy", 369.555 - (cos * 123.842));
+        //         um.setAttribute("r", 30);  // colocar o size
+        //         um.setAttribute("fill", yellow); // colocar a color
+
+        // // (estaduais) segundo circulo de fora - cx e cy posiçao inicial da bolinha no circulo de fora, calcula com o raio do circulo de fora
+        //         two.setAttribute("cx", 449.447 + (sen * 185.611));
+        //         two.setAttribute("cy", 369.552 - (cos * 185.611));
+        //         two.setAttribute("r", 15);  // colocar o size
+        //         two.setAttribute("fill", red);  // colocar a color
+
+        // // (exterior) terceiro circulo de fora - cx e cy posiçao inicial da bolinha no circulo de fora, calcula com o raio do circulo de fora
+        //         tres.setAttribute("cx", 450.219 + (sen * 247.746));
+        //         tres.setAttribute("cy", 368.363 - (cos * 247.746));
+        //         tres.setAttribute("r", 10);  // colocar o size
+        //         tres.setAttribute("fill", green);  // colocar a color
+
+        // // (privadas) quarto circulo de fora - cx e cy posiçao inicial da bolinha no circulo de fora, calcula com o raio do circulo de fora
+        //         four.setAttribute("cx", 450.5 + (sen * 308.611));
+        //         four.setAttribute("cy", 368.5 - (cos * 308.611));
+        //         four.setAttribute("r", 5); // colocar o size
+        //         four.setAttribute("fill", blue);  // colocar a color
+
+        // // movimento
+        //         myTimer.innerHTML = parseInt(angle/360*100) + '%';
+        //         myTimer.setAttribute("fill", "hsl(" + parseInt(angle/360*100) + ", 90%, 45%)");
+
+        //         if (angle >= max_angle) {
+        //             window.clearInterval(window.timer);
+        //         };
+
+        //         angle += angle_increment;
+        //     }.bind(this), interval);
+        },
+    },
+});
 
 
-  // declaracao de algumas variaveis necessarias
-  var blue = "rgba(0, 0, 255, 0.5)",
-      red = "rgba(255, 0, 0, 0.5)",
-      yellow = "rgba(255, 255, 0, 0.5)",
-      purple = "rgba(194, 74, 255, 0.5)",
-      green = "rgba(0, 255, 0, 0.5)",
-      g;
-
-
-  (function () {
-      // math trick 2*pi*57 = 358, must be less than 360 degree
-      var pointer = document.getElementById('pointer');
-      var myTimer = document.getElementById('myTimer');
-
-      // movimento
-      var interval = 30;
-      var angle = 0;
-      var angle_increment = 6;
-
-// id de cada circle - puxar da tabela
-      var two = document.getElementById('two');
-      var tres = document.getElementById('tres');
-      var four = document.getElementById('four');
-      var um = document.getElementById('um');
-
-// posição final das bolinhas - premios
-      var degrees = degrees; // puxar da tabela
-      var num = 100*2.15; // degrees * 2.15 - trocar o 100 por degrees
-      var max_angle = Math.round(num); // posição final da bolinha
-
-// tamanho da bolinha - artigos
-      var size = size; // puxar da tabela
-    //  var numsize = 50*0.14;  // size * 0.14 - trocar o 50 por size
-    //  var finalsize = Math.round(numsize); // tamanho da bolinha
-
-// cor da bolinha
-      var color = color; // puxar da tabela
-
-      window.timer = window.setInterval(function () {
-          var cos = Math.cos(angle/180*Math.PI);
-          var sen = Math.sin(angle/180*Math.PI);
-         //  console.log(angle)
-         //  console.log(cos)
-
-// (federais) primeiro circulo de fora - cx e cy posiçao inicial da bolinha no circulo de fora, calcula com o raio do circulo de fora
-         um.setAttribute("cx", 450.504 + (sen * 123.842));
-         um.setAttribute("cy", 369.555 - (cos * 123.842));
-         um.setAttribute("r", 30);  // colocar o size
-         um.setAttribute("fill", yellow); // colocar a color
-
-// (estaduais) segundo circulo de fora - cx e cy posiçao inicial da bolinha no circulo de fora, calcula com o raio do circulo de fora
-         two.setAttribute("cx", 449.447 + (sen * 185.611));
-         two.setAttribute("cy", 369.552 - (cos * 185.611));
-         two.setAttribute("r", 15);  // colocar o size
-         two.setAttribute("fill", red);  // colocar a color
-
-// (exterior) terceiro circulo de fora - cx e cy posiçao inicial da bolinha no circulo de fora, calcula com o raio do circulo de fora
-         tres.setAttribute("cx", 450.219 + (sen * 247.746));
-         tres.setAttribute("cy", 368.363 - (cos * 247.746));
-         tres.setAttribute("r", 10);  // colocar o size
-         tres.setAttribute("fill", green);  // colocar a color
-
-// (privadas) quarto circulo de fora - cx e cy posiçao inicial da bolinha no circulo de fora, calcula com o raio do circulo de fora
-          four.setAttribute("cx", 450.5 + (sen * 308.611));
-          four.setAttribute("cy", 368.5 - (cos * 308.611));
-          four.setAttribute("r", 5); // colocar o size
-          four.setAttribute("fill", blue);  // colocar a color
-
-// movimento
-          myTimer.innerHTML = parseInt(angle/360*100) + '%';
-          myTimer.setAttribute("fill", "hsl(" + parseInt(angle/360*100) + ", 90%, 45%)");
-
-          if (angle >= max_angle) {
-              window.clearInterval(window.timer);
-          };
-
-          angle += angle_increment;
-      }.bind(this), interval);
-  })();
