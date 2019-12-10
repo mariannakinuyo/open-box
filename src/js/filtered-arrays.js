862cf792a5e5f0950fdb8809bee89191cf5f27c5
Henrique Junges <henrique@rckt.com.br>
2019-12-10 12:01:20 -0300

    filtros laterais

--- src/js/filtered-arrays.js
+++ src/js/filtered-arrays.js
@@ -7,15 +7,27 @@ pessoas.nodes.forEach(e => {
     e.size = value;
     switch (e.type) {
         case 'federal':
+            var degrees = (e.degrees+13)/1.03;
+            e.cx = 450.219 + (Math.sin(degrees/180*Math.PI) * 247.746);
+            e.cy = 368.363 - (Math.cos(degrees/180*Math.PI) * 247.746);
             arrayFederal.push(e);
             break;
         case 'estadual':
+            var degrees = (e.degrees+10);
+            e.cx = 450.5 + (Math.sin(degrees/180*Math.PI) * 308.611);
+            e.cy = 368.5 - (Math.cos(degrees/180*Math.PI) * 308.611);
             arrayEstadual.push(e);
             break;
         case 'privada':
+            var degrees = (e.degrees/1.2)+27;
+            e.cx = 450.504 + (Math.sin(degrees/180*Math.PI) * 123.842);
+            e.cy = 369.555 - (Math.cos(degrees/180*Math.PI) * 123.842);
             arrayPrivada.push(e);
             break;
         case 'exterior':
+            var degrees = (e.degrees+17)/1.07;
+            e.cx = 449.447 + (Math.sin(degrees/180*Math.PI) * 185.611);
+            e.cy = 369.552 - (Math.cos(degrees/180*Math.PI) * 185.611);
             arrayExterior.push(e);
             break;
         default:
--- src/js/script-top.js
+++ src/js/script-top.js
@@ -11,6 +11,11 @@ var app = new Vue({
             exatas: true,
             engenharias: true,
             saude: true,
+            sul: true,
+            sudeste: true,
+            nordeste: true,
+            centro: true,
+            norte: true,
         },
     },
 
@@ -25,19 +30,19 @@ var app = new Vue({
         compFederal() {
             var vetor = [];
             this.arrayFederal.forEach((e) => {
-                if (e.color == 'rgba(0, 184, 104, 0.5)' && this.filter.biologicas) {
+                if (this.filter.biologicas && e.color == 'rgba(0, 184, 104, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(224, 79, 46, 0.5)' && this.filter.sociais) {
+                if (this.filter.sociais && e.color == 'rgba(224, 79, 46, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(245, 235, 53, 0.5)' && this.filter.exatas) {
+                if (this.filter.exatas && e.color == 'rgba(245, 235, 53, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(215, 64, 255, 0.5)' && this.filter.engenharias) {
+                if (this.filter.engenharias && e.color == 'rgba(215, 64, 255, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(38, 135, 252, 0.5)' && this.filter.saude) {
+                if (this.filter.saude && e.color == 'rgba(38, 135, 252, 0.5)') {
                     vetor.push(e);
                 }
             });
@@ -47,19 +52,19 @@ var app = new Vue({
         compEstadual() {
             var vetor = [];
             this.arrayEstadual.forEach((e) => {
-                if (e.color == 'rgba(0, 184, 104, 0.5)' && this.filter.biologicas) {
+                if (this.filter.biologicas && e.color == 'rgba(0, 184, 104, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(224, 79, 46, 0.5)' && this.filter.sociais) {
+                if (this.filter.sociais && e.color == 'rgba(224, 79, 46, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(245, 235, 53, 0.5)' && this.filter.exatas) {
+                if (this.filter.exatas && e.color == 'rgba(245, 235, 53, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(215, 64, 255, 0.5)' && this.filter.engenharias) {
+                if (this.filter.engenharias && e.color == 'rgba(215, 64, 255, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(38, 135, 252, 0.5)' && this.filter.saude) {
+                if (this.filter.saude && e.color == 'rgba(38, 135, 252, 0.5)') {
                     vetor.push(e);
                 }
             });
@@ -69,19 +74,19 @@ var app = new Vue({
         compPrivada() {
             var vetor = [];
             this.arrayPrivada.forEach((e) => {
-                if (e.color == 'rgba(0, 184, 104, 0.5)' && this.filter.biologicas) {
+                if (this.filter.biologicas && e.color == 'rgba(0, 184, 104, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(224, 79, 46, 0.5)' && this.filter.sociais) {
+                if (this.filter.sociais && e.color == 'rgba(224, 79, 46, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(245, 235, 53, 0.5)' && this.filter.exatas) {
+                if (this.filter.exatas && e.color == 'rgba(245, 235, 53, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(215, 64, 255, 0.5)' && this.filter.engenharias) {
+                if (this.filter.engenharias && e.color == 'rgba(215, 64, 255, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(38, 135, 252, 0.5)' && this.filter.saude) {
+                if (this.filter.saude && e.color == 'rgba(38, 135, 252, 0.5)') {
                     vetor.push(e);
                 }
             });
@@ -91,19 +96,19 @@ var app = new Vue({
         compExterior() {
             var vetor = [];
             this.arrayExterior.forEach((e) => {
-                if (e.color == 'rgba(0, 184, 104, 0.5)' && this.filter.biologicas) {
+                if (this.filter.biologicas && e.color == 'rgba(0, 184, 104, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(224, 79, 46, 0.5)' && this.filter.sociais) {
+                if (this.filter.sociais && e.color == 'rgba(224, 79, 46, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(245, 235, 53, 0.5)' && this.filter.exatas) {
+                if (this.filter.exatas && e.color == 'rgba(245, 235, 53, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(215, 64, 255, 0.5)' && this.filter.engenharias) {
+                if (this.filter.engenharias && e.color == 'rgba(215, 64, 255, 0.5)') {
                     vetor.push(e);
                 }
-                if (e.color == 'rgba(38, 135, 252, 0.5)' && this.filter.saude) {
+                if (this.filter.saude && e.color == 'rgba(38, 135, 252, 0.5)') {
                     vetor.push(e);
                 }
             });
@@ -112,101 +117,6 @@ var app = new Vue({
     },
 
     methods: {
-        filterGreen() {
-            let novo = [];
-            if (!this.filter.biologicas) { // incluir
-                novo = this.arrayFederal;
-                arrayFederal.forEach((e) => {
-                    if (e.color == 'rgba(0, 184, 104, 0.5)') {
-                        novo.push(e);
-                    }
-                });
-                this.arrayFederal = novo;
-                novo = this.arrayEstadual;
-                this.arrayEstadual.forEach((e) => {
-                    if (e.color == 'rgba(0, 184, 104, 0.5)') {
-                        novo.push(e);
-                    }
-                });
-                this.arrayEstadual = novo;
-                novo = this.arrayPrivada;
-                this.arrayPrivada.forEach((e) => {
-                    if (e.color == 'rgba(0, 184, 104, 0.5)') {
-                        novo.push(e);
-                    }
-                });
-                this.arrayPrivada = novo;
-                novo = this.arrayExterior;
-                arrayExterior.forEach((e) => {
-                    if (e.color == 'rgba(0, 184, 104, 0.5)') {
-                        novo.push(e);
-                    }
-                });
-                this.arrayExterior = novo;
-                novo = [];
-            } else { // remover rgba(0, 184, 104, 0.5)
-                this.arrayFederal.forEach((e) => {
-                    if (e.color != 'rgba(0, 184, 104, 0.5)') {
-                        novo.push(e);
-                    }
-                });
-                this.arrayFederal = novo;
-                novo = [];
-                this.arrayEstadual.forEach((e) => {
-                    if (e.color != 'rgba(0, 184, 104, 0.5)') {
-                        novo.push(e);
-                    }
-                });
-                this.arrayEstadual = novo;
-                novo = [];
-                this.arrayPrivada.forEach((e) => {
-                    if (e.color != 'rgba(0, 184, 104, 0.5)') {
-                        novo.push(e);
-                    }
-                });
-                this.arrayPrivada = novo;
-                novo = [];
-                this.arrayExterior.forEach((e) => {
-                    if (e.color != 'rgba(0, 184, 104, 0.5)') {
-                        novo.push(e);
-                    }
-                });
-                this.arrayExterior = novo;
-                novo = [];
-            }
-        },
-
-        filterRed() {
-            if (this.filter.sociais) {
-                // incluir
-            } else {
-                // remover rgba(224, 79, 46, 0.5)
-            }
-        },
-
-        filterYellow() {
-            if (this.filter.exatas) {
-                // incluir
-            } else {
-                // remover rgba(245, 235, 53, 0.5)
-            }
-        },
-
-        filterPurple() {
-            if (this.filter.engenharias) {
-                // incluir
-            } else {
-                // remover rgba(215, 64, 255, 0.5)
-            }
-        },
-
-        filterBlue() {
-            if (this.filter.saude) {
-                // incluir
-            } else {
-                // remover rgba(38, 135, 252, 0.5)
-            }
-        },
 
     },
 });
